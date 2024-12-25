fcb78cd302f959c35e8b7236f0667f2c3a2ddec3
Siddharth <siddharthrbhetariya@gamil.com>
2024-12-25 17:36:28 +0530

    mlflow started to work

--- backend/demo/src/main/java/com/MLops_project/demo/controller/testcontroller.java
+++ backend/demo/src/main/java/com/MLops_project/demo/controller/testcontroller.java
@@ -1,5 +1,6 @@
 package com.MLops_project.demo.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,7 +8,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class testcontroller {
@@ -16,14 +18,14 @@ public class testcontroller {
     public String home(){
         return "index";
     }
-
+    private static final Logger logger = LoggerFactory.getLogger(testcontroller.class);
     @Autowired
     private WebClient.Builder webClientBuilder;
     @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/predict")
     public Mono<ResponseEntity<String>> predict(@RequestBody String data) {
-        String pythonServiceUrl = "http://localhost:5000/predict";
-
+        String pythonServiceUrl = "http://app:5011/predict";
+        logger.info("Received data for prediction: {}", data);
         return webClientBuilder.build()
                 .post()
                 .uri(pythonServiceUrl)
